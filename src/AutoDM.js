dbfaaf32394e34e03e53c3959183d4a6f9e4fe55
rizuki0502 <rizki0502@student.undip.ac.id>
2020-04-02 16:03:52 +0700

    Initial commit

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `Hai ${name} \n Makasih udah ngefollow Kiki^^ \n Kiki orangnya autofolback kok, tapi kalau dia lupa tolong diingetin yaa! \n Happy ${dayName} 😊😊 \n \n -Kiko (Kiki's Bot)`; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
